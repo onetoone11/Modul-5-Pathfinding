656219270659df8c7e2dee5b51deafe39f25bd55
Max Hansen <max.hansen@learnet.se>
2021-11-26 09:44:29 +0100

    created world class

--- rooms.js
+++ rooms.js
@@ -47,4 +47,54 @@ class Room {
         this.name = names();
         this.exits = exits;
     }
+
+    name() {
+        return this.name;
+    }
+
+    exits() {
+        return this.exits;
+    }
+
+    id() {
+        return this.id;
+    }
+}
+
+const mod = (a,b) => {
+    return a - b * Math.floor(a/b);
+}
+
+class World {
+    constructor(name, length) {
+        this.name = name;
+        this.rooms = {};
+        this.length = length;
+    }
+
+    length() {
+        return this.rooms.length;
+    }
+
+    createStringRooms() {
+        for(let i = 0; i < this.length; i++) {
+            let temp = new Room(i, [(i+1) === this.length ? null : i+1, (i-1) === -1 ? null : i-1].filter(element => element !== null));
+            this.rooms[temp.id] = temp;
+        }
+    }
+
+    createCircularRooms() {
+        for(let i = 0; i < this.length; i++) {
+            let temp = new Room(i, [mod(i+1, this.length), mod(i-1, this.length)]);
+            this.rooms[temp.id] = temp;
+        }
+    }
+
+    createSquareRoom(sideLength) {
+        
+    }
+
+    getRoom(id) {
+        return this.rooms[id];
+    }
 }
