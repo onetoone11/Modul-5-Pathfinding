59c0b89ab67c17c75f6a3af7a34bb723c47ea937
Max Hansen <max.hansen@learnet.se>
2021-12-02 14:43:34 +0100

    fixed database

--- laravelproject/pathfinding-app/resources/views/pages/canvas.blade.php
+++ laravelproject/pathfinding-app/resources/views/pages/canvas.blade.php
@@ -7,12 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="{{ URL::asset('/css/canvas.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 
 </head>
 
 <body>
     <div id="app">
-        <form id="create_world" class="form-group">
+        {{-- <form id="create_world" class="form-group">
             <p>Type</p>
             <div>
                 <input class="radio_select radio" type="radio" id="string_select" name="world_type">
@@ -41,7 +42,11 @@
             <input type="number" id="complexity_input" placeholder="Complexity">
 
             <input type="button" id="create_button" value="Create">
-        </form>
+        </form> --}}
+
+        {!! Form::open(['action' => 'App\Http\Controllers\WorldsController@update', 'method' => 'PUT']) !!}
+        {!! Form::radio('type', 'string', true) !!}
+        {!! Form::close() !!}
         <canvas id="canvas" width="1000" height="800" class="border border-dark"></canvas>
 
 
