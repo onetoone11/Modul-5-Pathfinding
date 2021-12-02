c7007a6b17276f4670cd5922703dc828e6ae1ef1
Max Hansen <max.hansen@learnet.se>
2021-12-02 09:56:46 +0100

    db test

--- laravelproject/pathfinding-app/routes/web.php
+++ laravelproject/pathfinding-app/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
+
+Route::resource('pages','App\Http\Controllers\PagesController');
