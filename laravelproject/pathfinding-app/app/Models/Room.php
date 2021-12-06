7dfedb8e9916c92715d9318458ae8c196a2eb923
Max Hansen <max.hansen@learnet.se>
2021-12-06 10:05:41 +0100

    added several things. fixed canvas

--- laravelproject/pathfinding-app/app/Models/Room.php
+++ laravelproject/pathfinding-app/app/Models/Room.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+    
+    public function world() {
+        return $this->belongsTo(World::class);
+    }
 }
--- laravelproject/pathfinding-app/resources/views/layouts/app.blade.php
+++ laravelproject/pathfinding-app/resources/views/layouts/app.blade.php
@@ -6,8 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Module 5 Pathfinding</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+
 </head>
 <body>
     @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js">
 </body>
 </html>
